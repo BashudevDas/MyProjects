ae94d33c382d3f6592c9d8774690ff788dde4566
BashudevDas <138104086+BashudevDas@users.noreply.github.com>
2023-08-03 03:31:22 +0530

    Create PDF Merger.py
    Used to merge two or more PDFs. Code written using Python

---
+++ PDF Merger.py
@@ -0,0 +1,13 @@
+#PDF Merger 
+#For resume
+
+import PyPDF2
+import sys
+import os
+
+merger = PyPDF2.PdfFileMerger()
+
+for file in os.listdir(os.curdir):
+  if file.endswith(".pdf"):
+    merger.append(file)
+  merger.write("combinedPDF.pdf")
