cda0dfa636b50818243d25c4731e891fb24e3eb4
Bashudev Das <138104086+BashudevDas@users.noreply.github.com>
2023-08-13 19:41:20 +0530

    QR Code Generator.py
    A QR Code Generator made using Python Programming Language

--- QR Code Generator.py
+++ QR Code Generator.py
@@ -1,6 +1,14 @@
 #QR Code generator 
 #Made using Python
 
-import qrcode as qr
-img = qr.make("") #put the link of your desired website in here, starting with https
-img.save("")#save the file with any desired filename with .png extension
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,
+                   box_size=10,border=4)
+
+qr.add_data("")#put link of your desired website in here
+qr.make(fit=True)
+
+img = qr.make_image(fill_color = "green",back_color="blue")
+img.save("")#save your file with desired filename with .png extension
